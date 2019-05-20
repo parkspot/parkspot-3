b74685ef1585cd7c548321d6878dabd3f8bfb434
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-05-20 10:09:52 +0200

    file structure added

---
+++ app/parkspot_app/app/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <MapView
+        style={{flex: 1}}
+        region={{
+          latitude: 42.882004,
+          longitude: 74.582748,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        showsUserLocation={true}
+      />
+    );
+  }
+}
