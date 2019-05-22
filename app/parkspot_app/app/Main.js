ca737dd631d58b570366a2447cad337d64402136
nawatend <32384185+nawatend@users.noreply.github.com>
2019-05-22 21:43:22 +0200

    cleaned up

--- app/parkspot_app/app/Main.js
+++ app/parkspot_app/app/Main.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import MapView from 'react-native-maps'
-
-import Map from './components/Map'
 import HomeScreen from './views/HomeScreen'
 
 export default class Main extends React.Component {
