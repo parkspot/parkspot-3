aa3f3af298e2b3c50557246a73323e58409e530f
nawatend <32384185+nawatend@users.noreply.github.com>
2019-06-16 23:09:38 +0200

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -5,3 +5,10 @@ ScrollReveal().reveal('.text');
 ScrollReveal().reveal('img', {
     delay: 200
 });
+
+ScrollReveal().reveal('.slogan', {
+    delay: 300
+});
+ScrollReveal().reveal('.head', {
+    delay: 250
+});
